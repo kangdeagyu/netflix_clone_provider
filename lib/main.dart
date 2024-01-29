1ff01e7c39d415e059eb99166e69f18417de7cfb
kangdeagyu <rkdeorb0124@naver.com>
2024-01-29 18:03:48 +0900

    firebase yaml 추가

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:netflix_provider/firebase_options.dart';
 import 'package:netflix_provider/view/splash/splashpage.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
